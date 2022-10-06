58dce947ce93d8d3ee33ff9592e9fe7b193c916e
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2022-10-06 17:26:25 +0200

    add responde and request builder tests

---
+++ src/test/java/org/example/server/util/RequestBuilderTest.java
@@ -0,0 +1,38 @@
+package org.example.server.util;
+
+import org.example.server.dto.Request;
+import org.example.server.exception.UnsupportedProtocolException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestBuilderTest {
+
+    @Test
+    void testBuild() throws UnsupportedProtocolException {
+        // Arrange
+        String httpRequest =
+                """
+                POST /hello HTTP/1.1
+                Authorization: Basic hello-world-token
+                Content-Type: application/json
+                User-Agent: PostmanRuntime/7.28.3
+                Accept: */*
+                Host: localhost:10001
+                Accept-Encoding: gzip, deflate, br
+                Connection: keep-alive
+                Content-Length: 29
+                                
+                {
+                    "message": "World!"
+                }
+                """;
+
+        // Act
+        Request request = RequestBuilder.build(httpRequest);
+
+        // Assert
+        assertEquals(29, request.getContentLength());
+    }
+
+}
