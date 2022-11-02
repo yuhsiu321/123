586b8f6bbd260738325cf197d7ab0e620c62fe0f
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2022-11-02 12:36:23 +0100

    add jackson and user controller/model/repository

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.application.demo.DemoApp;
 import org.example.application.housing.HousingApp;
+import org.example.application.socialmedia.SocialMediaApp;
 import org.example.application.waiting.WaitingApp;
 import org.example.server.Server;
 
@@ -9,7 +10,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(new WaitingApp());
+        Server server = new Server(new SocialMediaApp());
         try {
             server.start();
         } catch (IOException e) {
